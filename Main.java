68a654f0965a3743f0b2e4669c2eed2ae6c744f0
Bruno Barros <brunog.barros@proton.me>
2023-08-03 22:44:03 -0300

    explicando classe Main

--- Main.java
+++ Main.java
@@ -1,4 +1,11 @@
+//visibilidade - publico
+// class = o que é 
+// nome = Main
 public class Main {
+    // visibilidade
+    // retorno (void)
+    // nome
+    // static = opcional, nem todo metodo é estático
     public static void main(String[] args) {
         System.out.println("Ola mundo TIKTOK PROGRAMA!");
     }
