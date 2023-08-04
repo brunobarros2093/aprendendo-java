e41b3ca5744bde0537d7393dc1a25d0e6b504ceb
Bruno Barros <brunog.barros@proton.me>
2023-08-03 22:51:59 -0300

    condicionais

---
+++ Condicionais.java
@@ -0,0 +1,21 @@
+public class Condicionais {
+    // Toda classe nossa possui um método main
+    // para que possamos executa-la!
+    public static void main(String[] args) {
+        int idade = 17;
+        // if = se
+        // else = senão
+        // else if = senão se
+        if (idade >= 18) { // se true - logica booleana
+            System.out.println("Pode comprar bebida");
+        } else if (idade <= 17) {
+            System.out.println("Não pode comprar bebida!");
+        } else {
+            System.out.println("Pode tudo pq não validei nada");
+        }
+        // > = maior
+        // < = menor
+        // <= = menor e igual
+        // >= maior e igual
+    }
+}
