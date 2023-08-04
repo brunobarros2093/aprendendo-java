e49a535f547f9addf1e6d2e0fac9aa77702dfb58
Bruno Barros <brunog.barros@proton.me>
2023-08-03 22:17:50 -0300

    video variaveis

---
+++ Variaveis.java
@@ -0,0 +1,21 @@
+public class Variaveis {
+    public static void main(String[] args) {
+        // isso é um comentário de uma linha
+        // que sera ignorado pelo compilador
+        // TIPOS BASICOS
+        int numeroInteiro = 10;
+        short numeroInteiroPequeno = 1;
+        long numeroBemGrande = 45649879874654L;
+        double numeroGrande = 312312;
+        float numeroComCasasDecimais = 32.0f; // 32.0
+        Boolean verdadeiroOuFalso = true;
+        char umCaractere = '1';
+        System.out.println("Inteiro: " + numeroInteiro +
+                "\nshort: " + numeroInteiroPequeno +
+                "\nlong: " + numeroBemGrande +
+                "\ndouble: " + numeroGrande +
+                "\nfloat:" + numeroComCasasDecimais +
+                "\nboolean:" + verdadeiroOuFalso +
+                "\nchar:" + umCaractere);
+    }
+}
