b27f11d51a63e2c566934f27f7470415d1fffd89
Bruno Barros <techpordentro@gmail.com>
2023-08-25 07:49:41 -0300

    Foor loop

---
+++ forloop.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+       List<String> nomes = new ArrayList<>();
+       nomes.add("Bruno");
+       nomes.add("Tiktok");
+       nomes.add("Aprendendo Java");
+       for(int i = 0; i < nomes.size(); i++) {
+        System.out.println(nomes);
+       }
+    }
+}
